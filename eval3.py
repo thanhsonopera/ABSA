12ec24f791de6892638fc4caf7fa8c8086e1c02c
Rye1606Zz <49356827+thanhsonopera@users.noreply.github.com>
2024-08-05 21:02:00 +0700

    up

--- eval3.py
+++ eval3.py
@@ -11,5 +11,5 @@ def aspect_eval(y_test, y_pred, num):
 
     aspect_report = classification_report(
         y_test, y_pred, digits=4, zero_division=1, output_dict=True)
-    with open('aspect_detection_report_{}.json'.format(num), 'w') as f:
+    with open('result/aspect_detection_report_{}.json'.format(num), 'w') as f:
         json.dump(aspect_report, f)
