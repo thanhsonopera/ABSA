c12e1ca1602e7691f03d07683447cfe6f0c5703c
Rye1606Zz <49356827+thanhsonopera@users.noreply.github.com>
2024-08-05 20:56:21 +0700

    up first

---
+++ eval3.py
@@ -0,0 +1,15 @@
+
+from sklearn.metrics import classification_report
+import json
+
+
+def aspect_eval(y_test, y_pred, num):
+    """
+    y_test: grouth_true test, DataFrame
+    y_pred: grouth_true predict, DataFrame
+    """
+
+    aspect_report = classification_report(
+        y_test, y_pred, digits=4, zero_division=1, output_dict=True)
+    with open('aspect_detection_report_{}.json'.format(num), 'w') as f:
+        json.dump(aspect_report, f)
